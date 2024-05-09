29e3a1780bdda6f9980495e1cbcfc37d28fae774
VladGrigorita <vladgrigorita2001@gmail.com>
2024-05-09 15:25:51 +0300

    update on notification email system

--- notification/src/main/java/com/example/notification/kafka/consumer/NotificationListener.java
+++ notification/src/main/java/com/example/notification/kafka/consumer/NotificationListener.java
@@ -3,6 +3,7 @@ package com.example.notification.kafka.consumer;
 import com.example.notification.dto.NotificationDTO;
 import com.example.notification.mapper.NotificationMapper;
 import com.example.notification.repository.NotificationRepository;
+import com.example.notification.service.MailService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -14,14 +15,18 @@ public class NotificationListener {
 
     private final NotificationMapper notificationMapper;
 
-    public NotificationListener(NotificationRepository notificationRepository, NotificationMapper notificationMapper) {
+    private final MailService mailService;
+
+    public NotificationListener(NotificationRepository notificationRepository, NotificationMapper notificationMapper, MailService mailService) {
         this.notificationRepository = notificationRepository;
         this.notificationMapper = notificationMapper;
+        this.mailService = mailService;
     }
 
     @KafkaListener(topics = "notification")
     public void listenForCanceledBookings(NotificationDTO notificationDTO) {
         notificationDTO.setCreatedAt(new Date());
+        mailService.sendEmail(notificationDTO.getReceiverEmail(), "Notification Email", notificationDTO.getMessage());
         notificationRepository.save(notificationMapper.toEntity(notificationDTO));
     }
 }
